7d19c4f918b90bb4cb5d267e8f5b51476f7fd706
Samuel Levi <samuellevi.job@gmail.com>
2025-07-10 20:03:23 -0300

    fixing supported size for videos

--- src/main/java/br/com/fiap/hackathon/adapter/controller/UploadVideoController.java
+++ src/main/java/br/com/fiap/hackathon/adapter/controller/UploadVideoController.java
@@ -1,15 +1,15 @@
 package br.com.fiap.hackathon.adapter.controller;
 
-import br.com.fiap.hackathon.adapter.presenter.dto.upload.UploadVideoRequest;
 import br.com.fiap.hackathon.domain.service.VideoValidationService;
 import br.com.fiap.hackathon.usecase.UploadVideoUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -24,9 +24,8 @@ public class UploadVideoController {
     public final UploadVideoUseCase uploadVideoUseCase;
 
     @PostMapping("/upload")
-    public ResponseEntity<String> uploadVideo(@RequestBody UploadVideoRequest uploadVideoRequest) {
-
-        String originalName = uploadVideoRequest.getVideoFile().getOriginalFilename();
+    public ResponseEntity<String> uploadVideo(@RequestPart("videoFile") MultipartFile videoFile, String email) {
+        String originalName = videoFile.getOriginalFilename();
         if (originalName == null || !videoValidationService.isValidVideoFile(originalName)) {
             return ResponseEntity.badRequest().body("Formato de arquivo inválido. Use: mp4, avi, mov, mkv");
         }
@@ -34,8 +33,8 @@ public class UploadVideoController {
         try {
             String id = UUID.randomUUID().toString();
             Path tempFile = Files.createTempFile(id, ".mp4");
-            uploadVideoRequest.getVideoFile().transferTo(tempFile.toFile());
-            uploadVideoUseCase.uploadVideo(id, tempFile, uploadVideoRequest.getEmail());
+            videoFile.transferTo(tempFile.toFile());
+            uploadVideoUseCase.uploadVideo(id, tempFile, email);
             Files.deleteIfExists(tempFile);
             return ResponseEntity.ok().build();
         } catch (Exception e) {
