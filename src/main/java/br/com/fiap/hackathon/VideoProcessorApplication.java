38f2cf5caaafab18c47a8f139a2c86f1b5ac4528
Samuel Levi <samuellevi.job@gmail.com>
2025-07-11 15:50:15 -0300

    log max file size

--- src/main/java/br/com/fiap/hackathon/VideoProcessorApplication.java
+++ src/main/java/br/com/fiap/hackathon/VideoProcessorApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VideoProcessorApplication {
     public static void main(String[] args) {
+        System.out.println("UPLOAD LIMIT: " +
+                System.getProperty("spring.servlet.multipart.max-file-size"));
         SpringApplication.run(VideoProcessorApplication.class);
     }
 }
