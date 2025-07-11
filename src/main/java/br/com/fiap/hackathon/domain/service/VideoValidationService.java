04bf2f715a289efb6530dabb816050c7d2769494
Samuel Levi <samuellevi.job@gmail.com>
2025-07-11 20:36:51 -0300

    apply sonarqube

--- src/main/java/br/com/fiap/hackathon/domain/service/VideoValidationService.java
+++ src/main/java/br/com/fiap/hackathon/domain/service/VideoValidationService.java
@@ -10,7 +10,7 @@ public class VideoValidationService {
     public boolean isValidVideoFile(String filename) {
         int dotIndex = filename.lastIndexOf(".");
         if (dotIndex == -1 || dotIndex == filename.length() - 1) {
-            return false; // Sem extensão/extensão vazia
+            return false;
         }
         String ext = filename.substring(dotIndex).toLowerCase();
         return List.of(".mp4", ".avi", ".mov", ".mkv", ".wmv", ".flv", ".webm").contains(ext);
